02f2209c97faa9cb8aaa3ce4571a795e92c7777f
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-11-06 16:57:08 +0900

    ABC299,300 B

--- abc/abc291-300/abc299/b.cpp
+++ abc/abc291-300/abc299/b.cpp
@@ -10,4 +10,17 @@ int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, t;
+  cin >> n >> t;
+  vector<int> c(n), r(n);
+  rep(i, 0, n) cin >> c[i];
+  rep(i, 0, n) cin >> r[i];
+  int color = t;
+  bool exist = false;
+  rep(i, 0, n) if (c[i] == t) exist = true;
+  if (!exist)
+    color = c[0];
+  int ans = -1, best = -1;
+  rep(i, 0, n) if (c[i] == color and r[i] > best) best = r[i], ans = i + 1;
+  cout << ans << endl;
 }
--- abc/abc291-300/abc300/b.cpp
+++ abc/abc291-300/abc300/b.cpp
@@ -6,8 +6,26 @@ using P = pair<int, int>;
 using ll = long long;
 const ll inf = 1LL << 60;
 
+vector<int> di = {-1, -1, 1, 1}, dj = {-1, 1, -1, 1};
+
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int h, w;
+  cin >> h >> w;
+  vector<string> a(h), b(h);
+  rep(i, 0, h) cin >> a[i];
+  rep(i, 0, h) cin >> b[i];
+  rep(s, 0, h) rep(t, 0, w)
+  {
+    vector<string> na = a;
+    rep(i, 0, h) rep(j, 0, w) na[(i + s) % h][(j + t) % w] = a[i][j];
+    if (na == b)
+    {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
 }

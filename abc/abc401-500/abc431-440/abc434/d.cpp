3bf0c7ec8adfc988ad276d6454aed9601c12486a
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-11-08 13:28:56 +0900

    remake ABC431-440

--- abc/abc401-500/abc431-440/abc434/d.cpp
+++ abc/abc401-500/abc431-440/abc434/d.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
 #define all(p) p.begin(), p.end()
+using P = pair<int, int>;
 using ll = long long;
 const ll inf = 1LL << 60;
 

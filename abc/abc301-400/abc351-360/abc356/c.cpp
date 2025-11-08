2c3175947dc57f9f7c0713defdfcc7bb050ea3dc
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-11-08 15:42:39 +0900

    ABC356,357 C

--- abc/abc301-400/abc351-360/abc356/c.cpp
+++ abc/abc301-400/abc351-360/abc356/c.cpp
@@ -1,9 +1,51 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define all(p) p.begin(), p.end()
+using P = pair<int, int>;
+using ll = long long;
+const ll inf = 1LL << 60;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m, k;
+  cin >> n >> m >> k;
+  vector<vector<int>> a(m);
+  vector<bool> r(m);
+  rep(i, 0, m)
+  {
+    int c;
+    cin >> c;
+    a[i].resize(c);
+    rep(j, 0, c)
+    {
+      cin >> a[i][j];
+      a[i][j]--;
+    }
+    char ri;
+    cin >> ri;
+    r[i] = (ri == 'o');
+  }
+  ll ans = 0;
+  rep(bit, 0, 1 << n)
+  {
+    bool ok = true;
+    rep(i, 0, m)
+    {
+      int keys = 0;
+      for (int j : a[i])
+        if (bit & (1 << j))
+          keys++;
+      if ((keys < k and r[i]) or (keys >= k and !r[i]))
+      {
+        ok = false;
+        break;
+      }
+    }
+    if (ok)
+      ans++;
+  }
+  cout << ans << endl;
 }
--- abc/abc301-400/abc351-360/abc357/c.cpp
+++ abc/abc301-400/abc351-360/abc357/c.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define all(p) p.begin(), p.end()
+using P = pair<int, int>;
+using ll = long long;
+const ll inf = 1LL << 60;
+
+vector<string> levelup(vector<string> s)
+{
+  int n = s.size();
+  vector<string> ans(n * 3);
+  rep(i, 0, 3 * n) rep(j, 0, 3 * n)
+  {
+    if (i >= n and i < n * 2 and j >= n and j < n * 2)
+      ans[i] += ".";
+    else
+      ans[i] += s[i % n][j % n];
+  }
+  return ans;
+}
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<string> ans(1, "#");
+  rep(i, 0, n) ans = levelup(ans);
+  for (string s : ans)
+    cout << s << endl;
 }

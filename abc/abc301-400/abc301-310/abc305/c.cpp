c9e42e9dd9b44970a683f61ce2f078d5a93f7810
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-11-26 12:07:50 +0900

    ABC305-306 C

--- abc/abc301-400/abc301-310/abc305/c.cpp
+++ abc/abc301-400/abc301-310/abc305/c.cpp
@@ -1,8 +1,31 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define all(p) p.begin(), p.end()
+using P = pair<int, int>;
 using ll = long long;
+const ll inf = 1LL << 60;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int h, w;
+  cin >> h >> w;
+  vector<string> s(h);
+  rep(i, 0, h) cin >> s[i];
+  int lx = w + 1, ly = h + 1, rx = -1, ry = -1;
+  rep(i, 0, h) rep(j, 0, w)
+  {
+    if (s[i][j] == '#')
+      ly = min(ly, i), lx = min(lx, j), ry = max(ry, i), rx = max(rx, j);
+  }
+  rep(i, ly, ry + 1) rep(j, lx, rx + 1)
+  {
+    if (s[i][j] == '.')
+    {
+      cout << i + 1 << ' ' << j + 1 << endl;
+      return 0;
+    }
+  }
 }
--- abc/abc301-400/abc301-310/abc306/c.cpp
+++ abc/abc301-400/abc301-310/abc306/c.cpp
@@ -1,8 +1,28 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define all(p) p.begin(), p.end()
+using P = pair<int, int>;
 using ll = long long;
+const ll inf = 1LL << 60;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  map<int, vector<int>> pos;
+  rep(i, 0, 3 * n)
+  {
+    int a;
+    cin >> a;
+    pos[a].push_back(i);
+  }
+  vector<P> ans;
+  rep(i, 1, n + 1) ans.push_back(make_pair(pos[i][1], i));
+  sort(all(ans));
+  for (auto [_, i] : ans)
+    cout << i << ' ';
+  cout << endl;
 }

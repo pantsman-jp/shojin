693e886488c2ba2e4cf3c3a80856e42fc9e83bf2
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-12-08 11:00:01 +0900

    ABC254 C

--- abc/abc201-300/abc251-260/abc254/c.cpp
+++ abc/abc201-300/abc251-260/abc254/c.cpp
@@ -10,4 +10,23 @@ int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, k;
+  cin >> n >> k;
+  vector<int> a(n);
+  rep(i, 0, n) cin >> a[i];
+  rep(r, 0, k)
+  {
+    vector<int> b;
+    for (int i = r; i < n; i += k)
+      b.push_back(a[i]);
+    sort(all(b));
+    for (int i = r; i < n; i += k)
+      a[i] = b[i / k];
+  }
+  vector<int> tmp = a;
+  sort(all(tmp));
+  if (a == tmp)
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
 }

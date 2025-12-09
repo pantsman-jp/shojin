ffe615c5efa015b5139758788310c17d49cac4df
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-12-09 15:39:28 +0900

    ABC240 C

--- abc/abc201-300/abc231-240/abc240/c.cpp
+++ abc/abc201-300/abc231-240/abc240/c.cpp
@@ -10,4 +10,21 @@ int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, x;
+  cin >> n >> x;
+  vector<int> a(n), b(n);
+  rep(i, 0, n) cin >> a[i] >> b[i];
+  vector<vector<bool>> dp(n + 1, vector<bool>(x + 1));
+  dp[0][0] = true;
+  rep(i, 0, n) rep(j, 0, x + 1)
+  {
+    if (j - a[i] >= 0 and dp[i][j - a[i]])
+      dp[i + 1][j] = true;
+    if (j - b[i] >= 0 and dp[i][j - b[i]])
+      dp[i + 1][j] = true;
+  }
+  if (dp[n][x])
+    cout << "Yes" << endl;
+  else
+    cout << "No" << endl;
 }

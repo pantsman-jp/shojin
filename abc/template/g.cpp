1fccdc3d31b5d9c3ab5322f60d6a5d1c94be5ff3
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-12-11 10:25:12 +0900

    improve template

--- abc/template/g.cpp
+++ abc/template/g.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
 #define all(p) p.begin(), p.end()
 using ll = long long;
+using P = pair<int, int>;
 const ll inf = 1LL << 60;
 
 int main()

9030efaebc4fc62dd4affb4fae20b9cc1f4acb69
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-12-11 10:45:47 +0900

    improve template

--- abc/template/e.cpp
+++ abc/template/e.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
 #define all(p) p.begin(), p.end()
+#define yn(p) cout << (p ? "Yes" : "No") << '\n';
 using ll = long long;
 using P = pair<int, int>;
 const ll inf = 1LL << 60;

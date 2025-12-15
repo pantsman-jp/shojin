9ead48deebae01ba4af9e8e451fc708ed419474d
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-12-15 11:40:54 +0900

    ABC181,182 C

--- abc/abc101-200/abc181-190/abc181/c.cpp
+++ abc/abc101-200/abc181-190/abc181/c.cpp
@@ -10,4 +10,17 @@ int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> x(n), y(n);
+  rep(i, 0, n) cin >> x[i] >> y[i];
+  rep(i, 0, n) rep(j, i + 1, n) rep(k, j + 1, n)
+  {
+    if ((x[k] - x[j]) * (y[j] - y[i]) == (x[j] - x[i]) * (y[k] - y[j]))
+    {
+      cout << "Yes" << endl;
+      return 0;
+    }
+  }
+  cout << "No" << endl;
 }
--- abc/abc101-200/abc181-190/abc182/c.cpp
+++ abc/abc101-200/abc181-190/abc182/c.cpp
@@ -6,8 +6,35 @@ using P = pair<int, int>;
 using ll = long long;
 const ll inf = 1LL << 60;
 
+int solve(ll n)
+{
+  vector<int> a;
+  while (n > 0)
+    a.push_back(n % 10), n /= 10;
+  int ans = 1e9, k = a.size();
+  rep(bit, 0, 1 << k)
+  {
+    int sum = 0, cnt = 0;
+    rep(i, 0, k)
+    {
+      if (bit & (1 << i))
+        cnt++;
+      else
+        sum += a[i];
+    }
+    if (cnt < k and sum % 3 == 0)
+      ans = min(ans, cnt);
+  }
+  if (ans < 1e9)
+    return ans;
+  return -1;
+}
+
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  ll n;
+  cin >> n;
+  cout << solve(n) << endl;
 }

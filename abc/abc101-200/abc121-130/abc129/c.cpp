bea555ffcff0919c9729954846c2be69e645e4d4
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-12-16 16:50:40 +0900

    ABC129,130 C

--- abc/abc101-200/abc121-130/abc129/c.cpp
+++ abc/abc101-200/abc121-130/abc129/c.cpp
@@ -5,9 +5,32 @@ using namespace std;
 using P = pair<int, int>;
 using ll = long long;
 const ll inf = 1LL << 60;
+const ll mod = 1e9 + 7;
 
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  vector<bool> broken(n + 1);
+  rep(_, 0, m)
+  {
+    int a;
+    cin >> a;
+    broken[a] = true;
+  }
+  vector<ll> dp(n + 1);
+  dp[0] = 1;
+  if (!broken[1])
+    dp[1] = 1;
+  rep(i, 2, n + 1)
+  {
+    if (!broken[i - 1])
+      dp[i] += dp[i - 1];
+    if (!broken[i - 2])
+      dp[i] += dp[i - 2];
+    dp[i] %= mod;
+  }
+  cout << dp[n] << endl;
 }
--- abc/abc101-200/abc121-130/abc130/c.cpp
+++ abc/abc101-200/abc121-130/abc130/c.cpp
@@ -4,10 +4,15 @@ using namespace std;
 #define all(p) p.begin(), p.end()
 using P = pair<int, int>;
 using ll = long long;
+using ld = long double;
 const ll inf = 1LL << 60;
 
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  ld w, h, x, y;
+  cin >> w >> h >> x >> y;
+  cout << fixed << setprecision(10) << w * h / 2.0;
+  cout << ' ' << ((w / 2.0 == x) and (h / 2.0 == y)) << endl;
 }

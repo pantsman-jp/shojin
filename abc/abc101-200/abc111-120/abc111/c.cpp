18f8dda050e571a3e1c5565e2af4fe8497e85b82
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-12-19 10:16:23 +0900

    ABC111,112 C

--- abc/abc101-200/abc111-120/abc111/c.cpp
+++ abc/abc101-200/abc111-120/abc111/c.cpp
@@ -10,4 +10,30 @@ int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i, 0, n) cin >> a[i];
+  vector<int> odd_hist(1e5 + 1, 0), even_hist(1e5 + 1, 0);
+  rep(i, 0, n)
+  {
+    if (i & 1)
+      odd_hist[a[i]]++;
+    else
+      even_hist[a[i]]++;
+  }
+  vector<P> odd, even;
+  for (int i = 0; i < 1e5 + 1; ++i)
+  {
+    odd.push_back(P(odd_hist[i], i));
+    even.push_back(P(even_hist[i], i));
+  }
+  sort(all(odd), greater<P>());
+  sort(all(even), greater<P>());
+  if (odd[0].second != even[0].second)
+    cout << n - (odd[0].first + even[0].first) << endl;
+  else
+    cout << n - max(odd[0].first + even[1].first,
+                    odd[1].first + even[0].first)
+         << endl;
 }
--- abc/abc101-200/abc111-120/abc112/c.cpp
+++ abc/abc101-200/abc111-120/abc112/c.cpp
@@ -10,4 +10,29 @@ int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, si = -1;
+  cin >> n;
+  vector<int> X(n), Y(n), H(n);
+  rep(i, 0, n)
+  {
+    cin >> X[i] >> Y[i] >> H[i];
+    if (H[i] > 0)
+      si = i;
+  }
+  int ansx = -1, ansy = -1, ansh = -1;
+  rep(x, 0, 101) rep(y, 0, 101)
+  {
+    int h = H[si] + abs(x - X[si]) + abs(y - Y[si]);
+    bool ok = true;
+    rep(i, 0, n)
+    {
+      if (H[i] > 0 and h - H[i] != abs(x - X[i]) + abs(y - Y[i]))
+        ok = false;
+      if (H[i] == 0 and h > abs(x - X[i]) + abs(y - Y[i]))
+        ok = false;
+    }
+    if (ok)
+      ansx = x, ansy = y, ansh = h;
+  }
+  cout << ansx << ' ' << ansy << ' ' << ansh << endl;
 }

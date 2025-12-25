e3639e4658a74ae8ff7652e6673ddd23335317ad
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-12-25 10:04:03 +0900

    ABC024 C

--- abc/abc001-100/abc021-030/abc024/c.cpp
+++ abc/abc001-100/abc021-030/abc024/c.cpp
@@ -10,4 +10,29 @@ int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, d, k;
+  cin >> n >> d >> k;
+  vector<int> l(d), r(d), s(k), t(k);
+  rep(i, 0, d) cin >> l[i] >> r[i];
+  rep(i, 0, k) cin >> s[i] >> t[i];
+  vector<int> ans(k);
+  rep(i, 0, d) rep(j, 0, k)
+  {
+    if (l[i] <= s[j] and s[j] <= r[i])
+    {
+      if (s[j] < t[j])
+      {
+        s[j] = min(t[j], r[i]);
+        if (s[j] == t[j])
+          ans[j] = i + 1;
+      }
+      else if (s[j] > t[j])
+      {
+        s[j] = max(t[j], l[i]);
+        if (s[j] == t[j])
+          ans[j] = i + 1;
+      }
+    }
+  }
+  rep(i, 0, k) cout << ans[i] << endl;
 }

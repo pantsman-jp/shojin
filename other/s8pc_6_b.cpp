d61bded74d0257c7f5466e1a34732831691fa1b6
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2025-12-27 12:04:11 +0900

    s8pc #6 B

---
+++ other/s8pc_6_b.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
+
+int main()
+{
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<ll> a(n), b(n);
+  rep(i, 0, n) cin >> a[i] >> b[i];
+  sort(all(a)), sort(all(b));
+  ll meda = a[n / 2], medb = b[n / 2], ans = 0;
+  rep(i, 0, n) ans += abs(meda - a[i]) + abs(a[i] - b[i]) + abs(b[i] - medb);
+  cout << ans << endl;
+}

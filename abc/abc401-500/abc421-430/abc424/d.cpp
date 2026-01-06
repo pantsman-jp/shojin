c738a8135457f5f516f2452270055685063630ab
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-06 15:11:58 +0900

    rewrite

--- abc/abc401-500/abc421-430/abc424/d.cpp
+++ abc/abc401-500/abc421-430/abc424/d.cpp
@@ -1,20 +1,22 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
 
 int main()
 {
-  int t;
-  cin >> t;
-  for (int i = 0; i < t; i++)
-  {
-    int h, w;
-    cin >> h >> w;
-    vector<string> grid(h);
-    for (int i = 0; i < h; i++)
-      cin >> grid[i];
-    vector<vector<int, int>> black;
-  }
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
 }

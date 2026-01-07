3628b338c610abae3686d228d0ccf9be32867d55
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-07 15:29:47 +0900

    typical90 002,010

--- typical90/p004.cpp
+++ typical90/p004.cpp
@@ -1,9 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
 #define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
-const ll inf = 1LL << 60;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
 
 int main()
 {
@@ -11,14 +21,17 @@ int main()
   cin.tie(nullptr);
   int h, w;
   cin >> h >> w;
-  vector<vector<int>> a(h, vector<int>(w));
+  vector a(h, vector<int>(w));
   rep(i, 0, h) rep(j, 0, w) cin >> a[i][j];
   vector<int> row(h), line(w);
-  rep(i, 0, h) row[i] = reduce(all(a[i]));
-  rep(j, 0, w) rep(i, 0, h) line[j] += a[i][j];
   rep(i, 0, h)
   {
-    rep(j, 0, w) { cout << row[i] + line[j] - a[i][j] << ' '; }
+    row[i] += accumulate(all(a[i]), 0);
+    rep(j, 0, w) line[j] += a[i][j];
+  }
+  rep(i, 0, h)
+  {
+    rep(j, 0, w) cout << row[i] + line[j] - a[i][j] << ' ';
     cout << endl;
   }
 }
--- typical90/p010.cpp
+++ typical90/p010.cpp
@@ -1,8 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
-const ll inf = 1LL << 60;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
 
 int main()
 {
@@ -10,23 +21,29 @@ int main()
   cin.tie(nullptr);
   int n;
   cin >> n;
-  vector<ll> sum1(n + 1), sum2(n + 1);
-  rep(i, 1, n + 1)
+  vector<int> p1(n), p2(n);
+  rep(i, 0, n)
   {
     int c, p;
     cin >> c >> p;
-    sum1[i] = sum1[i - 1], sum2[i] = sum2[i - 1];
     if (c == 1)
-      sum1[i] += p;
+      p1[i] = p;
     else
-      sum2[i] += p;
+      p2[i] = p;
+  }
+  vector<int> sum1(n + 1), sum2(n + 1);
+  rep(i, 0, n)
+  {
+    sum1[i + 1] = sum1[i] + p1[i];
+    sum2[i + 1] = sum2[i] + p2[i];
   }
   int q;
   cin >> q;
-  rep(i, 0, q)
+  rep(_, 0, q)
   {
     int l, r;
     cin >> l >> r;
-    cout << sum1[r] - sum1[l - 1] << ' ' << sum2[r] - sum2[l - 1] << '\n';
+    l--, r--;
+    cout << sum1[r + 1] - sum1[l] << ' ' << sum2[r + 1] - sum2[l] << endl;
   }
 }

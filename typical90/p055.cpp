4528b895f5932413503c5b9bbf3f802fbd8d038c
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-07 16:49:34 +0900

    typical90 033,055,061

--- typical90/p055.cpp
+++ typical90/p055.cpp
@@ -1,34 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
-const ll inf = 1LL << 60;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
 
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
-  ll n, p, q;
+  int n;
+  ll p, q;
   cin >> n >> p >> q;
   vector<ll> a(n);
   rep(i, 0, n) cin >> a[i];
-  ll ans = 0;
-  rep(i, 0, n)
+  int ans = 0;
+  rep(i, 0, n) rep(j, i + 1, n) rep(k, j + 1, n) rep(l, k + 1, n) rep(m, l + 1, n)
   {
-    rep(j, 0, i)
-    {
-      rep(k, 0, j)
-      {
-        rep(l, 0, k)
-        {
-          rep(m, 0, l)
-          {
-            if (a[i] * a[j] % p * a[k] % p * a[l] % p * a[m] % p == q)
-              ans++;
-          }
-        }
-      }
-    }
+    if (a[i] % p * a[j] % p * a[k] % p * a[l] % p * a[m] % p == q)
+      ans++;
   }
   cout << ans << endl;
 }
--- typical90/p061.cpp
+++ typical90/p061.cpp
@@ -1,22 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
-const ll inf = 1LL << 60;
-
-vector<int> deck;
-
-void solve()
-{
-  int t, x;
-  cin >> t >> x;
-  if (t == 1)
-    deck.insert(deck.begin(), x);
-  if (t == 2)
-    deck.push_back(x);
-  if (t == 3)
-    cout << deck[x - 1] << endl;
-}
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
 
 int main()
 {
@@ -24,5 +21,16 @@ int main()
   cin.tie(nullptr);
   int q;
   cin >> q;
-  rep(qi, 0, q) solve();
+  deque<int> deck;
+  rep(_, 0, q)
+  {
+    int t, x;
+    cin >> t >> x;
+    if (t == 1)
+      deck.push_front(x);
+    else if (t == 2)
+      deck.push_back(x);
+    else
+      cout << deck[x - 1] << endl;
+  }
 }

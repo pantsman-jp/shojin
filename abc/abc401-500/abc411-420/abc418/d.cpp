86514554530ee2b94814e15c98b718afc3a6b28a
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-13 15:04:38 +0900

    ABC418,419 D

--- abc/abc401-500/abc411-420/abc418/d.cpp
+++ abc/abc401-500/abc411-420/abc418/d.cpp
@@ -1,13 +1,36 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <string>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  string t;
+  cin >> n >> t;
+  vector<int> sum(n + 1, 0);
+  rep(i, 0, n) sum[i + 1] = sum[i] + (t[i] == '0');
+  ll even = 0, odd = 0;
+  rep(i, 0, n + 1)
+  {
+    if (sum[i] % 2 == 0)
+      even++;
+    else
+      odd++;
+  }
+  cout << even * (even - 1) / 2 + odd * (odd - 1) / 2 << endl;
 }
--- abc/abc401-500/abc411-420/abc419/d.cpp
+++ abc/abc401-500/abc411-420/abc419/d.cpp
@@ -1,13 +1,42 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <string>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  string s, t;
+  cin >> n >> m >> s >> t;
+  vector<int> swp(n + 1);
+  rep(_, 0, m)
+  {
+    int l, r;
+    cin >> l >> r;
+    l--;
+    swp[l]++, swp[r]--;
+  }
+  rep(i, 0, n) swp[i + 1] += swp[i];
+  rep(i, 0, n)
+  {
+    if (swp[i] % 2 == 0)
+      cout << s[i];
+    else
+      cout << t[i];
+  }
+  cout << endl;
 }

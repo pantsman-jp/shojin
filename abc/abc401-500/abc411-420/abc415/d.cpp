5e68493c3b696d233a450e7ebb9c8a31e161e65d
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-13 16:02:04 +0900

    ABC415,416 D

--- abc/abc401-500/abc411-420/abc415/d.cpp
+++ abc/abc401-500/abc411-420/abc415/d.cpp
@@ -1,13 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <string>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ll n;
+  int m;
+  cin >> n >> m;
+  vector<tuple<ll, ll, ll>> c(m);
+  rep(i, 0, m)
+  {
+    ll a, b;
+    cin >> a >> b;
+    c[i] = {a - b, a, b};
+  }
+  sort(all(c));
+  ll ans = 0;
+  for (auto [d, a, b] : c)
+  {
+    if (n < a)
+      continue;
+    ll cnt = max(0ll, (n - a) / d + 1);
+    ans += cnt;
+    n -= d * cnt;
+  }
+  cout << ans << endl;
 }
--- abc/abc401-500/abc411-420/abc416/d.cpp
+++ abc/abc401-500/abc411-420/abc416/d.cpp
@@ -1,13 +1,48 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <string>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
+
+void solve()
+{
+  int n;
+  ll m;
+  cin >> n >> m;
+  vector<ll> a(n), b(n);
+  rep(i, 0, n) cin >> a[i];
+  rep(i, 0, n) cin >> b[i];
+  sort(rall(a)), sort(all(b));
+  ll ans = 0;
+  rep(i, 0, n) ans += a[i] + b[i];
+  int i = 0;
+  for (ll ai : a)
+  {
+    while (i < n and ai + b[i] < m)
+      i++;
+    if (i == n)
+      break;
+    ans -= m, i++;
+  }
+  cout << ans << endl;
+}
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  rep(_, 0, t) solve();
 }

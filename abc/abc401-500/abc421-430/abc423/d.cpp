5ca0b94e5c83ca9f30839f4acdbb696655fddc51
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-13 10:23:13 +0900

    ABC423,424 D

--- abc/abc401-500/abc421-430/abc423/d.cpp
+++ abc/abc401-500/abc421-430/abc423/d.cpp
@@ -1,9 +1,46 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (ll i = 0; i < (n); i++)
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
-const ll inf = 1LL << 60;
+using ld = long double;
+using P = pair<ll, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, k;
+  cin >> n >> k;
+  priority_queue<P, vector<P>, greater<P>> pq;
+  ll empty = k, ans = 0;
+  rep(i, 0, n)
+  {
+    ll a, b, c;
+    cin >> a >> b >> c;
+    while (pq.size() and pq.top().first < a)
+    {
+      empty += pq.top().second;
+      pq.pop();
+    }
+    ans = max<ll>(ans, a);
+    while (empty < c)
+    {
+      ans = pq.top().first;
+      empty += pq.top().second;
+      pq.pop();
+    }
+    empty -= c;
+    pq.emplace(ans + b, c);
+    cout << ans << endl;
+  }
 }
--- abc/abc401-500/abc421-430/abc424/d.cpp
+++ abc/abc401-500/abc421-430/abc424/d.cpp
@@ -19,4 +19,49 @@ int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int t;
+  cin >> t;
+  rep(_, 0, t)
+  {
+    int h, w;
+    cin >> h >> w;
+    vector<string> s(h);
+    rep(i, 0, h) cin >> s[i];
+    vector<int> orig(h);
+    rep(i, 0, h)
+    {
+      int m = 0;
+      rep(j, 0, w) if (s[i][j] == '#') m |= (1 << j);
+      orig[i] = m;
+    }
+    int n = 1 << w;
+    vector dp(h + 1, vector<int>(n, inf));
+    rep(s0, 0, n) dp[1][s0] = __builtin_popcount(orig[0] & (~s0));
+    rep(i, 2, h + 1)
+    {
+      rep(p, 0, n)
+      {
+        if (dp[i - 1][p] == inf)
+          continue;
+        rep(s1, 0, n)
+        {
+          bool ok = true;
+          rep(j, 0, w - 1)
+          {
+            if ((p & (1 << j)) && (p & (1 << (j + 1))) and (s1 & (1 << j)) && (s1 & (1 << (j + 1))))
+            {
+              ok = false;
+              break;
+            }
+          }
+          if (!ok)
+            continue;
+          chmin(dp[i][s1], dp[i - 1][p] + __builtin_popcount(orig[i - 1] & (~s1)));
+        }
+      }
+    }
+    int ans = inf;
+    rep(j, 0, n) chmin(ans, dp[h][j]);
+    cout << ans << endl;
+  }
 }

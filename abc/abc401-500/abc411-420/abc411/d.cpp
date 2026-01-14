fb3f9ee08e7d8e55bde3aaa52ef5e87b1a0d3878
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-14 13:41:17 +0900

    ABC411-412 D

--- abc/abc401-500/abc411-420/abc411/d.cpp
+++ abc/abc401-500/abc411-420/abc411/d.cpp
@@ -1,13 +1,50 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <string>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, q;
+  cin >> n >> q;
+  vector<tuple<int, int, string>> qs;
+  rep(qi, 0, q)
+  {
+    int t, p;
+    string s;
+    cin >> t >> p;
+    if (t == 2)
+      cin >> s;
+    qs.emplace_back(t, p, s);
+  }
+  int pc = 0;
+  vector<string> ans;
+  reverse(all(qs));
+  for (auto [t, p, s] : qs)
+  {
+    if (t == 1 and pc == p)
+      pc = 0;
+    if (t == 2 and pc == p)
+      ans.push_back(s);
+    if (t == 3 and pc == 0)
+      pc = p;
+  }
+  reverse(all(ans));
+  for (string s : ans)
+    cout << s;
+  cout << endl;
 }
--- abc/abc401-500/abc411-420/abc412/d.cpp
+++ abc/abc401-500/abc411-420/abc412/d.cpp
@@ -1,13 +1,47 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <string>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  vector g(n, vector<int>(n));
+  rep(i, 0, m)
+  {
+    int a, b;
+    cin >> a >> b;
+    a--, b--;
+    g[a][b] = 1, g[b][a] = 1;
+  }
+  int ans = inf;
+  vector<int> p(n);
+  rep(i, 0, n) p[i] = i;
+  do
+  {
+    bool ok = true;
+    rep(i, 0, n) if (p[i] == i or p[p[i]] == i) ok = false;
+    if (ok)
+    {
+      int cnt = 0;
+      rep(i, 0, n) cnt += g[i][p[i]];
+      chmin(ans, n + m - cnt * 2);
+    }
+  } while (next_permutation(all(p)));
+  cout << ans << endl;
 }

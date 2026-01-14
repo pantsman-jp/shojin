da82b95bff092f3f0f47e27b468acc9d57394bcd
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-14 14:25:10 +0900

    ABC409-410 D

--- abc/abc401-500/abc401-410/abc409/d.cpp
+++ abc/abc401-500/abc401-410/abc409/d.cpp
@@ -1,8 +1,55 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-typedef long long ll;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
+
+void solve()
+{
+  int n;
+  string s;
+  cin >> n >> s;
+  int l = -1;
+  rep(i, 0, n - 1)
+  {
+    if (s[i] > s[i + 1])
+    {
+      l = i;
+      break;
+    }
+  }
+  if (l != -1)
+  {
+    int r = n - 1;
+    rep(i, l + 1, n - 1)
+    {
+      if (s[l] < s[i + 1])
+      {
+        r = i;
+        break;
+      }
+    }
+    rotate(s.begin() + l, s.begin() + (l + 1), s.begin() + (r + 1));
+  }
+  cout << s << endl;
+}
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int t;
+  cin >> t;
+  rep(_, 0, t) solve();
 }
--- abc/abc401-500/abc401-410/abc410/d.cpp
+++ abc/abc401-500/abc401-410/abc410/d.cpp
@@ -1,8 +1,66 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-typedef long long ll;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
+
+struct Edge
+{
+  int to, w;
+};
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  int W = 1 << 10;
+  vector<vector<Edge>> g(n);
+  rep(i, 0, m)
+  {
+    int a, b, w;
+    cin >> a >> b >> w;
+    a--, b--;
+    g[a].emplace_back(b, w);
+  }
+  vector<bool> visited(n * W);
+  queue<int> q;
+  auto push = [&](int v, int x)
+  {
+    int vid = v * W + x;
+    if (visited[vid])
+      return;
+    visited[vid] = true;
+    q.push(vid);
+  };
+  push(0, 0);
+  while (!q.empty())
+  {
+    int vid = q.front();
+    q.pop();
+    int v = vid / W, x = vid % W;
+    for (auto e : g[v])
+      push(e.to, x ^ e.w);
+  }
+  rep(x, 0, W)
+  {
+    if (visited[(n - 1) * W + x])
+    {
+      cout << x << endl;
+      return 0;
+    }
+  }
+  cout << -1 << endl;
 }

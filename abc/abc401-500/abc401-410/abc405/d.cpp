fd3e2f58508a047de9fc944dc4c0ba105f615a2e
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-15 13:18:42 +0900

    ABC405,406 D

--- abc/abc401-500/abc401-410/abc405/d.cpp
+++ abc/abc401-500/abc401-410/abc405/d.cpp
@@ -1,8 +1,80 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-typedef long long ll;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
+
+const vector<int> di = {-1, 0, 1, 0};
+const vector<int> dj = {0, 1, 0, -1};
+
+bool out(int i, int j, int h, int w)
+{
+  return i < 0 or h - 1 < i or j < 0 or w - 1 < j;
+}
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int h, w;
+  cin >> h >> w;
+  vector<string> s(h);
+  rep(i, 0, h) cin >> s[i];
+  queue<P> q;
+  vector seen(h, vector<bool>(w));
+  vector dist(h, vector<int>(w, inf));
+  rep(i, 0, h) rep(j, 0, w)
+  {
+    if (s[i][j] == 'E')
+    {
+      seen[i][j] = true;
+      dist[i][j] = 0;
+      q.push({i, j});
+    }
+  }
+  while (!q.empty())
+  {
+    auto [i, j] = q.front();
+    q.pop();
+    rep(d, 0, 4)
+    {
+      int ni = i + di[d], nj = j + dj[d];
+      if (!out(ni, nj, h, w) and !seen[ni][nj] and s[ni][nj] != '#')
+      {
+        seen[ni][nj] = true;
+        dist[ni][nj] = dist[i][j] + 1;
+        q.push({ni, nj});
+      }
+    }
+  }
+  vector ans = s;
+  string arrow = "^>v<";
+  rep(i, 0, h) rep(j, 0, w)
+  {
+    if (s[i][j] == '.')
+    {
+      rep(d, 0, 4)
+      {
+        int ni = i + di[d], nj = j + dj[d];
+        if (!out(ni, nj, h, w) and dist[ni][nj] == dist[i][j] - 1)
+        {
+          ans[i][j] = arrow[d];
+          break;
+        }
+      }
+    }
+  }
+  for (string t : ans)
+    cout << t << endl;
 }
--- abc/abc401-500/abc401-410/abc406/d.cpp
+++ abc/abc401-500/abc401-410/abc406/d.cpp
@@ -1,8 +1,55 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-typedef long long ll;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod =;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int h, w, n;
+  cin >> h >> w >> n;
+  vector<set<int>> row(h), line(w);
+  rep(i, 0, n)
+  {
+    int x, y;
+    cin >> x >> y;
+    x--, y--;
+    row[x].insert(y);
+    line[y].insert(x);
+  }
+  int q;
+  cin >> q;
+  rep(_, 0, q)
+  {
+    int t, p;
+    cin >> t >> p;
+    p--;
+    if (t == 1)
+    {
+      cout << row[p].size() << endl;
+      for (int j : row[p])
+        line[j].erase(p);
+      row[p] = set<int>();
+    }
+    else
+    {
+      cout << line[p].size() << endl;
+      for (int j : line[p])
+        row[j].erase(p);
+      line[p] = set<int>();
+    }
+  }
 }

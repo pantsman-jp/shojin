b15d235e2ded8f935613479981886eafbe9deaaf
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-19 12:03:23 +0900

    ABC393,394 D

--- abc/abc301-400/abc391-400/abc393/d.cpp
+++ abc/abc301-400/abc391-400/abc393/d.cpp
@@ -1,14 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <atcoder/all>
-using namespace atcoder;
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
 #define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
-const ll inf = 1LL << 60;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n;
+  string s;
+  cin >> n >> s;
+  vector<ll> pos;
+  rep(i, 0, n) if (s[i] == '1') pos.push_back(i);
+  int k = pos.size();
+  rep(i, 0, k) pos[i] -= i;
+  ll m = pos[k / 2], ans = 0;
+  rep(i, 0, k) ans += abs(pos[i] - m);
+  cout << ans << endl;
 }
--- abc/abc301-400/abc391-400/abc394/d.cpp
+++ abc/abc301-400/abc391-400/abc394/d.cpp
@@ -1,14 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
-#include <atcoder/all>
-using namespace atcoder;
 #define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
 #define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
-const ll inf = 1LL << 60;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  string s;
+  cin >> s;
+  stack<char> br;
+  for (char c : s)
+  {
+    if (c == '(' or c == '[' or c == '<')
+      br.push(c);
+    else
+    {
+      if (br.empty())
+      {
+        yn(false);
+        return 0;
+      }
+      char top = br.top();
+      if ((top == '(' and c == ')') or (top == '[' and c == ']') or (top == '<' and c == '>'))
+        br.pop();
+      else
+      {
+        yn(false);
+        return 0;
+      }
+    }
+  }
+  yn(br.empty());
 }

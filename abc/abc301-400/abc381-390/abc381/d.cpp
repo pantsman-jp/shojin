e4fb0fe8207ca102ccd01d45ffa22a66d6111326
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-20 17:01:00 +0900

    ABC381,382 D

--- abc/abc301-400/abc381-390/abc381/d.cpp
+++ abc/abc301-400/abc381-390/abc381/d.cpp
@@ -1,9 +1,66 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
+
+int n, ans = 0;
+vector<int> a;
+
+void solve(int start)
+{
+  vector<int> p;
+  for (int i = start; i + 1 < n; i += 2)
+  {
+    if (a[i] == a[i + 1])
+      p.push_back(a[i]);
+    else
+      p.push_back(-1);
+  }
+  vector<int> cnt(n + 1);
+  int l = 0;
+  rep(r, 0, p.size())
+  {
+    if (p[r] == -1)
+    {
+      while (l <= r)
+      {
+        if (p[l] != -1)
+          cnt[p[l]]--;
+        l++;
+      }
+      continue;
+    }
+    while (cnt[p[r]] > 0)
+    {
+      cnt[p[l]]--;
+      l++;
+    }
+    cnt[p[r]]++;
+    chmax(ans, (r - l + 1) * 2);
+  }
+}
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cin >> n;
+  a.resize(n);
+  rep(i, 0, n) cin >> a[i];
+  solve(0);
+  solve(1);
+  cout << ans << endl;
 }
--- abc/abc301-400/abc381-390/abc382/d.cpp
+++ abc/abc301-400/abc381-390/abc382/d.cpp
@@ -1,9 +1,56 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
+
+int n, m;
+vector<vector<int>> ans;
+
+void dfs(vector<int> &a)
+{
+  if ((int)a.size() == n)
+  {
+    ans.push_back(a);
+    return;
+  }
+  int last, remain = n - a.size() - 1, sup = m - 10 * remain;
+  if (a.empty())
+    last = 1;
+  else
+    last = a.back() + 10;
+  rep(v, last, sup + 1)
+  {
+    a.push_back(v);
+    dfs(a);
+    a.pop_back();
+  }
+}
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cin >> n >> m;
+  vector<int> a;
+  dfs(a);
+  cout << ans.size() << endl;
+  for (vector<int> xs : ans)
+  {
+    for (int x : xs)
+      cout << x << ' ';
+    cout << endl;
+  }
 }

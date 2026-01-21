66f7734b034861e7ad9f0e0aa8383789dd7ea1d6
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-21 16:17:59 +0900

    ABC371,372 D

--- abc/abc301-400/abc371-380/abc371/d.cpp
+++ abc/abc301-400/abc371-380/abc371/d.cpp
@@ -1,9 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> x(n);
+  rep(i, 0, n) cin >> x[i];
+  vector<ll> p(n);
+  rep(i, 0, n) cin >> p[i];
+  vector<ll> sum(n + 1);
+  rep(i, 0, n) sum[i + 1] = sum[i] + p[i];
+  int q;
+  cin >> q;
+  rep(_, 0, q)
+  {
+    int l, r;
+    cin >> l >> r;
+    int i = lower_bound(all(x), l) - x.begin();
+    int j = upper_bound(all(x), r) - x.begin();
+    cout << sum[j] - sum[i] << endl;
+  }
 }
--- abc/abc301-400/abc371-380/abc372/d.cpp
+++ abc/abc301-400/abc371-380/abc372/d.cpp
@@ -1,9 +1,41 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> h(n);
+  rep(i, 0, n) cin >> h[i];
+  stack<int> bld, ans;
+  rrep(i, n, 0)
+  {
+    ans.push(bld.size());
+    while (!bld.empty() and bld.top() < h[i])
+      bld.pop();
+    bld.push(h[i]);
+  }
+  while (!ans.empty())
+  {
+    cout << ans.top() << " ";
+    ans.pop();
+  }
+  cout << endl;
 }

d8a63de9325955eda8df4acacef7e72e9972da22
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-23 14:27:34 +0900

    ABC363,364 D

--- abc/abc301-400/abc361-370/abc363/d.cpp
+++ abc/abc301-400/abc361-370/abc363/d.cpp
@@ -1,9 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ll n;
+  cin >> n;
+  if (n == 1)
+  {
+    cout << 0 << endl;
+    return 0;
+  }
+  n--;
+  for (int digit = 1;; digit++)
+  {
+    ll num = 9;
+    rep(i, 0, (digit + 1) / 2 - 1) num *= 10;
+    if (n > num)
+    {
+      n -= num;
+      continue;
+    }
+    n += num / 9 - 1;
+    string s = to_string(n);
+    string t = s;
+    reverse(all(t));
+    if (digit % 2 == 1)
+      s.pop_back();
+    cout << s + t << endl;
+    return 0;
+  }
 }
--- abc/abc301-400/abc361-370/abc364/d.cpp
+++ abc/abc301-400/abc361-370/abc364/d.cpp
@@ -1,9 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, q;
+  cin >> n >> q;
+  vector<int> a(n);
+  rep(i, 0, n) cin >> a[i];
+  sort(all(a));
+  rep(_, 0, q)
+  {
+    int b, k;
+    cin >> b >> k;
+    int l = 0, r = 2e8 + 1;
+    while (l < r)
+    {
+      int mid = (l + r) / 2;
+      if (upper_bound(all(a), b + mid) - lower_bound(all(a), b - mid) >= k)
+        r = mid;
+      else
+        l = mid + 1;
+    }
+    cout << r << endl;
+  }
 }

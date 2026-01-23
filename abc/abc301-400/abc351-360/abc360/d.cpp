afcf41be7510b27719b7e290fc896ded55b259ef
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-23 15:23:22 +0900

    ABC360 D

--- abc/abc301-400/abc351-360/abc360/d.cpp
+++ abc/abc301-400/abc351-360/abc360/d.cpp
@@ -1,9 +1,49 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  ll t;
+  string s;
+  cin >> n >> t >> s;
+  vector<ll> xp, xn;
+  rep(i, 0, n)
+  {
+    ll x;
+    cin >> x;
+    if (s[i] == '1')
+      xp.push_back(x);
+    else
+      xn.push_back(x);
+  }
+  sort(all(xp)), sort(all(xn));
+  ll l = 0, r = 0, ans = 0;
+  int m = xn.size();
+  for (ll x : xp)
+  {
+    while (l < m and xn[l] < x)
+      l++;
+    while (r < m and xn[r] <= x + 2 * t)
+      r++;
+    ans += r - l;
+  }
+  cout << ans << endl;
 }

f87877ab2f7607c328ad35d3faedcf5bf78efa51
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-24 13:22:21 +0900

    ABC355,356 D

--- abc/abc301-400/abc351-360/abc355/d.cpp
+++ abc/abc301-400/abc351-360/abc355/d.cpp
@@ -1,9 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<P> pos(n);
+  rep(i, 0, n) cin >> pos[i].first >> pos[i].second;
+  sort(all(pos));
+  vector<int> ls;
+  rep(i, 0, n) ls.push_back(pos[i].first);
+  ll ans = 0;
+  rep(i, 0, n) ans += upper_bound(all(ls), pos[i].second) - ls.begin() - 1 - i;
+  cout << ans << endl;
 }
--- abc/abc301-400/abc351-360/abc356/d.cpp
+++ abc/abc301-400/abc351-360/abc356/d.cpp
@@ -1,9 +1,40 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  ll n, m;
+  cin >> n >> m;
+  n++;
+  ll ans = 0;
+  rep(i, 0, 60)
+  {
+    if (((m >> i) & 1) != 0)
+    {
+      ll cycle = 1LL << (i + 1);
+      ll count = (n / cycle % mod) * ((1LL << i) % mod) % mod;
+      ll rem = n % cycle;
+      if (rem > (1LL << i))
+        count = (count + (rem - (1LL << i))) % mod;
+      ans = (ans + count) % mod;
+    }
+  }
+  cout << ans << endl;
 }

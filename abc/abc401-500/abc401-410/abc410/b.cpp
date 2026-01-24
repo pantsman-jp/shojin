8ef64c98491695c0c4164e8f04a1224d10469c1e
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-24 12:37:48 +0900

    ABC410B

--- abc/abc401-500/abc401-410/abc410/b.cpp
+++ abc/abc401-500/abc401-410/abc410/b.cpp
@@ -1,8 +1,44 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
 using namespace std;
-typedef long long ll;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, q;
+  cin >> n >> q;
+  vector<int> box(n + 1);
+  rep(_, 0, q)
+  {
+    int x;
+    cin >> x;
+    if (x != 0)
+    {
+      box[x]++;
+      cout << x << " ";
+    }
+    else
+    {
+      int i = 1;
+      rep(j, 2, n + 1) if (box[i] > box[j]) i = j;
+      box[i]++;
+      cout << i << " ";
+    }
+  }
+  cout << endl;
 }

c9adfac46c5e0682f6d8b5e5ea0e8345e2ffbf33
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-27 16:49:07 +0900

    ABC327,328 D

--- abc/abc301-400/abc321-330/abc327/d.cpp
+++ abc/abc301-400/abc321-330/abc327/d.cpp
@@ -1,8 +1,55 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
-typedef long long ll;
+using namespace atcoder;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
+
+vector<vector<int>> g;
+vector<int> label;
+
+bool dfs(int u, int l)
+{
+  label[u] = l;
+  for (int v : g[u])
+  {
+    if (label[v] == l)
+      return false;
+    if (label[v] == -1 and !dfs(v, (l + 1) % 2))
+      return false;
+  }
+  return true;
+}
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  vector<int> a(m), b(m);
+  rep(i, 0, m) cin >> a[i], a[i]--;
+  rep(i, 0, m) cin >> b[i], b[i]--;
+  g.resize(n);
+  rep(i, 0, m)
+  {
+    g[a[i]].push_back(b[i]);
+    g[b[i]].push_back(a[i]);
+  }
+  label.resize(n, -1);
+  yn(dfs(0, 0));
 }
--- abc/abc301-400/abc321-330/abc328/d.cpp
+++ abc/abc301-400/abc321-330/abc328/d.cpp
@@ -1,8 +1,37 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
-typedef long long ll;
+using namespace atcoder;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string s;
+  cin >> s;
+  string ans;
+  for (char c : s)
+  {
+    ans.push_back(c);
+    int n = ans.size();
+    if (n >= 3 and ans[n - 3] == 'A' and ans[n - 2] == 'B' and ans[n - 1] == 'C')
+      rep(_, 0, 3) ans.pop_back();
+  }
+  cout << ans << endl;
 }

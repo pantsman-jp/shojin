6369d2050405b7e7fb851ffb1ae001d395d36f2b
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-27 16:07:20 +0900

    ABC329,330 D

--- abc/abc301-400/abc321-330/abc329/d.cpp
+++ abc/abc301-400/abc321-330/abc329/d.cpp
@@ -1,8 +1,45 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
-typedef long long ll;
+using namespace atcoder;
+#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(a, b) a = max(a, b)
+#define chmin(a, b) a = min(a, b)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  vector<int> cnt(n);
+  int maxim = 0, winner = -1;
+  rep(i, 0, m)
+  {
+    int a;
+    cin >> a;
+    a--;
+    cnt[a]++;
+    if (cnt[a] > maxim)
+    {
+      maxim = cnt[a];
+      winner = a;
+    }
+    else if (cnt[a] == maxim)
+      chmin(winner, a);
+    cout << winner + 1 << endl;
+  }
 }

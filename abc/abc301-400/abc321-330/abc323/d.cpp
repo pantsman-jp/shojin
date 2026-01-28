4a567ab3beba4927e49c17763a2a80e9711c3364
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-28 10:30:15 +0900

    ABC323,324 D

--- abc/abc301-400/abc321-330/abc323/d.cpp
+++ abc/abc301-400/abc321-330/abc323/d.cpp
@@ -1,8 +1,46 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
-typedef long long ll;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  map<ll, ll> cnt;
+  rep(_, 0, n)
+  {
+    ll s, c;
+    cin >> s >> c;
+    cnt[s] += c;
+  }
+  for (auto it = cnt.begin(); it != cnt.end(); it++)
+  {
+    ll s = it->first;
+    ll c = it->second;
+    cnt[s] = c % 2;
+    cnt[2 * s] += c / 2;
+  }
+  ll ans = 0;
+  for (auto [_, c] : cnt)
+    ans += c;
+  cout << ans << endl;
 }
--- abc/abc301-400/abc321-330/abc324/d.cpp
+++ abc/abc301-400/abc321-330/abc324/d.cpp
@@ -1,8 +1,40 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
-typedef long long ll;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  string s;
+  cin >> n >> s;
+  sort(all(s));
+  int ans = 0;
+  for (ll m = 0; m * m <= pow(10, n); m++)
+  {
+    string t = to_string(m * m);
+    t.resize(n, '0');
+    sort(all(t));
+    if (s == t)
+      ans++;
+  }
+  cout << ans << endl;
 }

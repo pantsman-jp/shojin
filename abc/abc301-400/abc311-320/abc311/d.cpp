4cf0eb1f6aa7e7d35ead5e94792150f9a1764145
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-28 15:15:15 +0900

    ABC311,312 D

--- abc/abc301-400/abc311-320/abc311/d.cpp
+++ abc/abc301-400/abc311-320/abc311/d.cpp
@@ -1,8 +1,66 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
+
+const vector<int> di = {-1, 0, 1, 0};
+const vector<int> dj = {0, 1, 0, -1};
+
+int n, m;
+vector<string> s;
+vector<vector<bool>> seen, is_start;
+
+bool out(int i, int j, int h, int w)
+{
+  return i < 0 or h - 1 < i or j < 0 or w - 1 < j;
+}
+
+void dfs(int i, int j)
+{
+  if (is_start[i][j])
+    return;
+  is_start[i][j] = true;
+  seen[i][j] = true;
+  rep(d, 0, 4)
+  {
+    int ni = i, nj = j;
+    while (s[ni + di[d]][nj + dj[d]] == '.')
+    {
+      ni += di[d];
+      nj += dj[d];
+      seen[ni][nj] = true;
+    }
+    dfs(ni, nj);
+  }
+}
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cin >> n >> m;
+  s.resize(n);
+  rep(i, 0, n) cin >> s[i];
+  seen.resize(n, vector<bool>(m));
+  is_start.resize(n, vector<bool>(m));
+  dfs(1, 1);
+  int ans = 0;
+  rep(i, 0, n) rep(j, 0, m) if (seen[i][j]) ans++;
+  cout << ans << endl;
 }
--- abc/abc301-400/abc311-320/abc312/d.cpp
+++ abc/abc301-400/abc311-320/abc312/d.cpp
@@ -1,8 +1,40 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+using mint = modint998244353;
+// using mint = modint1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string s;
+  cin >> s;
+  int n = s.size();
+  vector dp(n + 1, vector<mint>(n + 1));
+  dp[0][0] = 1;
+  rep(i, 0, n) rep(j, 0, n)
+  {
+    if (s[i] != ')')
+      dp[i + 1][j + 1] += dp[i][j];
+    if (s[i] != '(' and j != 0)
+      dp[i + 1][j - 1] += dp[i][j];
+  }
+  cout << dp[n][0].val() << endl;
 }

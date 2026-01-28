2677df22237ad21f8682870371217fdde6db0a1d
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-28 12:42:02 +0900

    ABC319,320 D

--- abc/abc301-400/abc311-320/abc319/d.cpp
+++ abc/abc301-400/abc311-320/abc319/d.cpp
@@ -1,8 +1,47 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  vector<ll> l(n);
+  rep(i, 0, n) cin >> l[i], ++l[i];
+  ll left = *max_element(all(l)) - 1, right = accumulate(all(l), 0LL);
+  while (left + 1 < right)
+  {
+    ll mid = (left + right) / 2;
+    ll row = 1, len = 0;
+    rep(i, 0, n)
+    {
+      len += l[i];
+      if (len > mid)
+        row++, len = l[i];
+    }
+    if (row > m)
+      left = mid;
+    else
+      right = mid;
+  }
+  cout << right - 1 << endl;
 }
--- abc/abc301-400/abc311-320/abc320/d.cpp
+++ abc/abc301-400/abc311-320/abc320/d.cpp
@@ -1,8 +1,60 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
+
+vector<vector<tuple<int, ll, ll>>> g;
+vector<bool> seen;
+vector<pair<ll, ll>> pos;
+
+void dfs(int u, ll x, ll y)
+{
+  seen[u] = true;
+  pos[u] = {x, y};
+  for (auto [v, dx, dy] : g[u])
+    if (!seen[v])
+      dfs(v, x + dx, y + dy);
+}
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  g.resize(n);
+  rep(_, 0, m)
+  {
+    int a, b;
+    ll x, y;
+    cin >> a >> b >> x >> y;
+    a--, b--;
+    g[a].push_back({b, x, y});
+    g[b].push_back({a, -x, -y});
+  }
+  seen.resize(n), pos.resize(n);
+  dfs(0, 0, 0);
+  rep(i, 0, n)
+  {
+    if (seen[i])
+      cout << pos[i].first << " " << pos[i].second << endl;
+    else
+      cout << "undecidable" << endl;
+  }
 }

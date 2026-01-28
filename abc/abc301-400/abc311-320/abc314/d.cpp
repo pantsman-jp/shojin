b51e18f257c7f914e97824f4e4f88ff61445710b
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-28 14:08:13 +0900

    ABC314 D

--- abc/abc301-400/abc311-320/abc314/d.cpp
+++ abc/abc301-400/abc311-320/abc314/d.cpp
@@ -1,8 +1,56 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, q;
+  string s;
+  cin >> n >> s >> q;
+  vector<int> lt(n);
+  int last = -1, type = 2;
+  rep(i, 0, q)
+  {
+    int t, x;
+    char c;
+    cin >> t >> x >> c;
+    x--;
+    if (t == 1)
+    {
+      s[x] = c;
+      lt[x] = i;
+    }
+    else
+    {
+      last = i;
+      type = t;
+    }
+  }
+  rep(i, 0, n) if (lt[i] < last)
+  {
+    if (type == 2)
+      s[i] = tolower(s[i]);
+    else
+      s[i] = toupper(s[i]);
+  }
+  cout << s << endl;
 }

d929a48a358407c85e31c5e6ec80236ee04f27c9
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-28 15:16:54 +0900

    improve template

--- abc/template/a.cpp
+++ abc/template/a.cpp
@@ -12,12 +12,13 @@ using namespace atcoder;
 #define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
 using ld = long double;
+using ull = unsigned long long;
 using P = pair<int, int>;
 const int inf = 1 << 30;
 const ll INF = 1LL << 60;
 const ld pi = acosl(-1.0L);
-// const ll mod = 998244353;
-// const ll mod = 1000000007;
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
 int main()
 {

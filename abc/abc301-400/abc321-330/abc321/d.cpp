a25025a08d650c1138416dcd57594934270a6c9e
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-28 10:49:47 +0900

    ABC321 D

--- abc/abc301-400/abc321-330/abc321/d.cpp
+++ abc/abc301-400/abc321-330/abc321/d.cpp
@@ -1,8 +1,42 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
-typedef long long ll;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+using ll = long long;
+using ld = long double;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// const ll mod = 998244353;
+// const ll mod = 1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  ll p;
+  cin >> n >> m >> p;
+  vector<ll> a(n), b(m);
+  rep(i, 0, n) cin >> a[i];
+  rep(i, 0, m) cin >> b[i];
+  sort(all(a)), sort(all(b));
+  vector<ll> sum(m + 1);
+  rep(i, 0, m) sum[i + 1] = sum[i] + b[i];
+  ll ans = 0;
+  for (ll x : a)
+  {
+    int i = upper_bound(all(b), p - x) - b.begin();
+    ans += (m - i) * p + x * i + sum[i];
+  }
+  cout << ans << endl;
 }

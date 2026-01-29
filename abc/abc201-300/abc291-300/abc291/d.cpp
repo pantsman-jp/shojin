74db22d25c127466f267060c3fcac73c671e0035
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-29 16:12:59 +0900

    ABC291,292 D

--- abc/abc201-300/abc291-300/abc291/d.cpp
+++ abc/abc201-300/abc291-300/abc291/d.cpp
@@ -1,13 +1,45 @@
 #include <bits/stdc++.h>
+#include <atcoder/all>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
-const ll inf = 1LL << 60;
+using ld = long double;
+using ull = unsigned long long;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+using mint = modint998244353;
+// using mint = modint1000000007;
 
 int main()
 {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> a(n), b(n);
+  rep(i, 0, n) cin >> a[i] >> b[i];
+  vector dp(n, vector<mint>(2));
+  dp[0][0] = dp[0][1] = 1;
+  rep(i, 1, n)
+  {
+    if (a[i] != a[i - 1])
+      dp[i][0] += dp[i - 1][0];
+    if (a[i] != b[i - 1])
+      dp[i][0] += dp[i - 1][1];
+    if (b[i] != a[i - 1])
+      dp[i][1] += dp[i - 1][0];
+    if (b[i] != b[i - 1])
+      dp[i][1] += dp[i - 1][1];
+  }
+  cout << (dp[n - 1][0] + dp[n - 1][1]).val() << endl;
 }

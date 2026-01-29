cf115bf6229fdd89d4d5dc1a181734a1cf0b5ed1
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-29 11:50:33 +0900

    ABC301,302 D

--- abc/abc301-400/abc301-310/abc301/d.cpp
+++ abc/abc301-400/abc301-310/abc301/d.cpp
@@ -1,8 +1,47 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string s;
+  ll n;
+  cin >> s >> n;
+  int m = s.size();
+  ll ans = 0;
+  rep(i, 0, m)
+  {
+    if (s[i] == '1')
+      ans += 1LL << (m - i - 1);
+    else if (s[i] == '?')
+    {
+      ll tmp = ans + (1LL << (m - i - 1));
+      rep(j, i + 1, m) if (s[j] == '1') tmp += 1LL << (m - j - 1);
+      if (tmp <= n)
+        ans += 1LL << (m - i - 1);
+    }
+  }
+  if (ans > n)
+    ans = -1;
+  cout << ans << endl;
 }
--- abc/abc301-400/abc301-310/abc302/d.cpp
+++ abc/abc301-400/abc301-310/abc302/d.cpp
@@ -1,8 +1,42 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m;
+  ll d;
+  cin >> n >> m >> d;
+  vector<ll> a(n), b(m);
+  rep(i, 0, n) cin >> a[i];
+  rep(i, 0, m) cin >> b[i];
+  sort(all(a)), sort(all(b));
+  ll ans = -1;
+  for (ll x : a)
+  {
+    int i = upper_bound(all(b), x + d) - b.begin() - 1;
+    if (i >= 0 and abs(x - b[i]) <= d)
+      chmax(ans, x + b[i]);
+  }
+  cout << ans << endl;
 }

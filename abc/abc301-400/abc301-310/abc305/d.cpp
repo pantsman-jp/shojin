64e2b5369e975177b621d1bf5579ee2a5e1d4d0e
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-29 09:36:18 +0900

    ABC305,306 D

--- abc/abc301-400/abc301-310/abc305/d.cpp
+++ abc/abc301-400/abc301-310/abc305/d.cpp
@@ -1,8 +1,55 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, q;
+  cin >> n;
+  vector<int> a(n);
+  rep(i, 0, n) cin >> a[i];
+  cin >> q;
+  vector<int> sum(n);
+  rep(i, 1, n)
+  {
+    sum[i] = sum[i - 1];
+    if (i % 2 == 0)
+      sum[i] += a[i] - a[i - 1];
+  }
+  auto f = [&](int x)
+  {
+    int i = lower_bound(all(a), x) - a.begin() - 1;
+    if (i < 0)
+      return 0;
+    int ans = sum[i];
+    if (i % 2 == 1)
+      ans += x - a[i];
+    return ans;
+  };
+  rep(_, 0, q)
+  {
+    int l, r;
+    cin >> l >> r;
+    cout << f(r) - f(l) << endl;
+  }
 }
--- abc/abc301-400/abc301-310/abc306/d.cpp
+++ abc/abc301-400/abc301-310/abc306/d.cpp
@@ -1,8 +1,47 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (int i = 0; i < (n); i++)
+#include <atcoder/all>
 using namespace std;
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
 int main()
 {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<vector<ll>> dp(2, vector<ll>(n + 1, -INF));
+  dp[0][0] = 0;
+  rep(i, 0, n)
+  {
+    int x;
+    ll y;
+    cin >> x >> y;
+    chmax(dp[0][i + 1], dp[0][i]);
+    chmax(dp[1][i + 1], dp[1][i]);
+    if (x == 0)
+    {
+      chmax(dp[0][i + 1], dp[0][i] + y);
+      chmax(dp[0][i + 1], dp[1][i] + y);
+    }
+    else
+      chmax(dp[1][i + 1], dp[0][i] + y);
+  }
+  cout << max(dp[0][n], dp[1][n]) << endl;
 }

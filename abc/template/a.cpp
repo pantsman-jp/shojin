0aca2e4bc1d7bebf49b3b850482c9242e0b252ac
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-30 12:46:31 +0900

    fix template

--- abc/template/a.cpp
+++ abc/template/a.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-#include <atcoder/all>
 using namespace std;
+#include <atcoder/all>
 using namespace atcoder;
 #define rep(i, l, r) for (int i = (l); i < (r); i++)
 #define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
@@ -20,8 +20,7 @@ const ld pi = acosl(-1.0L);
 // using mint = modint998244353;
 // using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   return 0;

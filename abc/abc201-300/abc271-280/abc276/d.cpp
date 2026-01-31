e188a95918234a24865ed8eafa403ff86d562b6b
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-31 12:43:32 +0900

    ABC275,276 D
    Co-authored-by: pantsman-jp <pantsman-jp@users.noreply.github.com>

--- abc/abc201-300/abc271-280/abc276/d.cpp
+++ abc/abc201-300/abc271-280/abc276/d.cpp
@@ -1,13 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
-const ll inf = 1LL << 60;
+using ld = long double;
+using ull = unsigned long long;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i, 0, n) cin >> a[i];
+  int g = 0;
+  rep(i, 0, n) g = gcd(g, a[i]);
+  int ans = 0;
+  rep(i, 0, n) {
+    a[i] /= g;
+    while (a[i] % 2 == 0) a[i] /= 2, ans++;
+    while (a[i] % 3 == 0) a[i] /= 3, ans++;
+    if (a[i] != 1) {
+      cout << -1 << endl;
+      return 0;
+    }
+  }
+  cout << ans << endl;
+  return 0;
 }

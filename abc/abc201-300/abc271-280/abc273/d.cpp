c4f509689fa427e4bba644ebe53fd3d02dfc3dfa
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-01-31 13:27:23 +0900

    ABC273,274 D

--- abc/abc201-300/abc271-280/abc273/d.cpp
+++ abc/abc201-300/abc271-280/abc273/d.cpp
@@ -1,13 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
-const ll inf = 1LL << 60;
+using ld = long double;
+using ull = unsigned long long;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;#include <bits/stdc++.h>
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int h, w, si, sj, n;
+  cin >> h >> w >> si >> sj >> n;
+  map<int, vector<int>> mpl, mpr, mpu, mpd;
+  rep(_, 0, n) {
+    int i, j;
+    cin >> i >> j;
+    mpr[i].push_back(j);
+    mpl[i].push_back(-j);
+    mpd[j].push_back(i);
+    mpu[j].push_back(-i);
+  }
+  auto init = [&](map<int, vector<int>>& mp, int r) {
+    for (auto& p : mp) {
+      sort(all(p.second));
+      p.second.push_back(r);
+    }
+  };
+  init(mpr, w + 1);
+  init(mpl, 0);
+  init(mpd, h + 1);
+  init(mpu, 0);
+  auto f = [&](map<int, vector<int>>& mp, int i, int j, int l, int r) {
+    auto it = mp.find(i);
+    if (it == mp.end()) return min(j + l, r - 1);
+    auto& is = it->second;
+    int wj = *lower_bound(all(is), j);
+    return min(j + l, wj - 1);
+  };
+  int q;
+  cin >> q;
+  rep(_, 0, q) {
+    char d;
+    int l;
+    cin >> d >> l;
+    if (d == 'R') sj = f(mpr, si, sj, l, w + 1);
+    if (d == 'L') sj = -f(mpl, si, -sj, l, 0);
+    if (d == 'D') si = f(mpd, sj, si, l, h + 1);
+    if (d == 'U') si = -f(mpu, sj, -si, l, 0);
+    cout << si << " " << sj << endl;
+  }
+  return 0;
 }
--- abc/abc201-300/abc271-280/abc274/d.cpp
+++ abc/abc201-300/abc271-280/abc274/d.cpp
@@ -1,13 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(p) cout << (p ? "Yes\n" : "No\n");
+#define dout(f) cout << fixed << setprecision(10) << f << '\n';
 using ll = long long;
-const ll inf = 1LL << 60;
+using ld = long double;
+using ull = unsigned long long;
+using P = pair<int, int>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+bool f(int x, vector<int> a) {
+  set<int> s;
+  s.insert(0);
+  for (int na : a) {
+    set<int> p;
+    swap(p, s);
+    for (int nx : p) {
+      s.insert(nx - na);
+      s.insert(nx + na);
+    }
+  }
+  return s.count(x) != 0;
+}
+
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, x, y;
+  cin >> n >> x >> y;
+  vector<int> a(n);
+  rep(i, 0, n) cin >> a[i];
+  vector<int> xs, ys;
+  rep(i, 1, n) {
+    if (i % 2 == 0)
+      xs.push_back(a[i]);
+    else
+      ys.push_back(a[i]);
+  }
+  yn(f(x - a[0], xs) and f(y, ys));
+  return 0;
 }

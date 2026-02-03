d796c3c87b38d60fac15e035775b43f9910a5423
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-03 13:18:53 +0900

    ABC241 D

--- abc/abc201-300/abc241-250/abc241/d.cpp
+++ abc/abc201-300/abc241-250/abc241/d.cpp
@@ -1,13 +1,67 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int q;
+  cin >> q;
+  multiset<ll> a;
+  rep(_, 0, q) {
+    int t, k;
+    ll x;
+    bool ok = true;
+    cin >> t;
+    if (t == 1) {
+      cin >> x;
+      a.insert(x);
+    } else if (t == 2) {
+      cin >> x >> k;
+      auto it = a.upper_bound(x);
+      rep(_, 0, k) {
+        if (it == a.begin())
+          ok = false;
+        else
+          it--;
+      }
+      if (ok)
+        cout << *it << endl;
+      else
+        cout << -1 << endl;
+    } else {
+      cin >> x >> k;
+      auto it = a.lower_bound(x);
+      rep(_, 0, k - 1) {
+        if (it == a.end())
+          ok = false;
+        else
+          it++;
+      }
+      if (ok and it != a.end())
+        cout << *it << endl;
+      else
+        cout << -1 << endl;
+    }
+  }
+  return 0;
 }

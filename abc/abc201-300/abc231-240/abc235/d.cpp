c2cace4cc23e3b861c4f27236fef25a99f8707c9
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-03 15:41:58 +0900

    ABC235,236 D

--- abc/abc201-300/abc231-240/abc235/d.cpp
+++ abc/abc201-300/abc231-240/abc235/d.cpp
@@ -1,13 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  ll a, n;
+  cin >> a >> n;
+  queue<ll> q;
+  vector<int> dist(10 * n, -1);
+  auto push = [&](ll nxt, ll now) -> void {
+    if (nxt >= n * 10) return;
+    if (dist[nxt] == -1) {
+      q.push(nxt);
+      dist[nxt] = dist[now] + 1;
+    }
+  };
+  q.push(1);
+  dist[1] = 0;
+  while (!q.empty()) {
+    ll x = q.front();
+    q.pop();
+    push(x * a, x);
+    if (x % 10 != 0) {
+      string sx = to_string(x);
+      sx = sx.back() + sx.substr(0, sx.size() - 1);
+      istringstream si(sx);
+      ll y;
+      si >> y;
+      push(y, x);
+    }
+  }
+  cout << dist[n] << endl;
+  return 0;
 }
--- abc/abc201-300/abc231-240/abc236/d.cpp
+++ abc/abc201-300/abc231-240/abc236/d.cpp
@@ -1,13 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int n, n2;
+vector<vector<int>> a;
+int ans;
+
+void dfs(vector<bool> paired, int x) {
+  int si = -1;
+  rep(i, 0, n2) if (!paired[i]) {
+    si = i;
+    break;
+  }
+  if (si == -1) {
+    chmax(ans, x);
+    return;
+  }
+  paired[si] = true;
+  rep(i, 0, n2) if (!paired[i] and si != i) {
+    paired[i] = true;
+    dfs(paired, x ^ a[si][i]);
+    paired[i] = false;
+  }
+}
+
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  cin >> n;
+  n2 = 2 * n;
+  a = vector<vector<int>>(n2, vector<int>(n2));
+  rep(i, 0, n2) rep(j, i + 1, n2) {
+    cin >> a[i][j];
+    a[j][i] = a[i][j];
+  }
+  vector<bool> paired(n2);
+  dfs(paired, 0);
+  cout << ans << endl;
+  return 0;
 }

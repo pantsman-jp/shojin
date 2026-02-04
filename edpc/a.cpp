53eb2027ef3e0a63191db15cf0c75a88c843bce2
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-04 15:50:57 +0900

    EDPC A,B

--- edpc/a.cpp
+++ edpc/a.cpp
@@ -1,30 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
-#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
 #define rall(p) p.rbegin(), p.rend()
-#define chmax(a, b) a = max(a, b)
-#define chmin(a, b) a = min(a, b)
-#define yn(p) cout << (p ? "Yes\n" : "No\n");
-#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
+using ull = unsigned long long;
 using ld = long double;
-using P = pair<int, int>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 const int inf = 1 << 30;
 const ll INF = 1LL << 60;
-// const ll mod =;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int n;
   cin >> n;
   vector<int> h(n);
   rep(i, 0, n) cin >> h[i];
-  vector<int> dp(n);
-  dp[0] = 0, dp[1] = abs(h[0] - h[1]);
-  rep(i, 2, n) dp[i] = min(dp[i - 2] + abs(h[i] - h[i - 2]), dp[i - 1] + abs(h[i] - h[i - 1]));
-  cout << dp.back() << endl;
+  vector<int> dp(n, inf);
+  dp[0] = 0;
+  rep(i, 1, n) {
+    chmin(dp[i], dp[i - 1] + abs(h[i] - h[i - 1]));
+    if (i > 1) chmin(dp[i], dp[i - 2] + abs(h[i] - h[i - 2]));
+  }
+  cout << dp[n - 1] << endl;
+  return 0;
 }
--- edpc/b.cpp
+++ edpc/b.cpp
@@ -1,40 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
-#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
 #define rall(p) p.rbegin(), p.rend()
-#define chmax(a, b) a = max(a, b)
-#define chmin(a, b) a = min(a, b)
-#define yn(p) cout << (p ? "Yes\n" : "No\n");
-#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
+using ull = unsigned long long;
 using ld = long double;
-using P = pair<int, int>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 const int inf = 1 << 30;
 const ll INF = 1LL << 60;
-// const ll mod =;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
   int n, k;
   cin >> n >> k;
   vector<int> h(n);
   rep(i, 0, n) cin >> h[i];
-  vector<int> dp(n);
-  dp[0] = 0, dp[1] = abs(h[0] - h[1]);
-  rep(i, 2, n)
-  {
-    int best = inf;
-    rep(j, 1, k + 1)
-    {
-      if (i - j < 0)
-        break;
-      chmin(best, dp[i - j] + abs(h[i] - h[i - j]));
-    }
-    dp[i] = best;
+  vector<int> dp(n, inf);
+  dp[0] = 0;
+  rep(i, 0, n) {
+    rep(j, i + 1, min(n, i + 1 + k)) chmin(dp[j], dp[i] + abs(h[i] - h[j]));
   }
-  cout << dp.back() << endl;
+  cout << dp[n - 1] << endl;
+  return 0;
 }

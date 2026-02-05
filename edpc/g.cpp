839e5f19c665411eca45587353962f54f090574e
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-05 12:59:14 +0900

    EDPC G,H

--- edpc/g.cpp
+++ edpc/g.cpp
@@ -1,13 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+vector<vector<int>> g;
+vector<int> dp;
+
+int dfs(int u) {
+  if (dp[u] != -1) return dp[u];
+  int ans = 0;
+  for (int v : g[u]) chmax(ans, dfs(v) + 1);
+  return dp[u] = ans;
+}
+
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, m;
+  cin >> n >> m;
+  g.assign(n, vector<int>());
+  rep(_, 0, m) {
+    int x, y;
+    cin >> x >> y;
+    x--, y--;
+    g[x].push_back(y);
+  }
+  int ans = 0;
+  dp.assign(n, -1);
+  rep(i, 0, n) chmax(ans, dfs(i));
+  cout << ans << endl;
+  return 0;
 }
--- edpc/h.cpp
+++ edpc/h.cpp
@@ -1,13 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+using mint = modint1000000007;
 
-int main()
-{
+const vector<int> di = {0, 1};
+const vector<int> dj = {1, 0};
+
+bool out(int i, int j, int h, int w) {
+  return i < 0 or h - 1 < i or j < 0 or w - 1 < j;
+}
+
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int h, w;
+  cin >> h >> w;
+  vector<string> a(h);
+  rep(i, 0, h) cin >> a[i];
+  vector dp(h, vector<mint>(w, mint{0}));
+  dp[0][0] = 1;
+  rep(i, 0, h) rep(j, 0, w) rep(d, 0, 2) {
+    int ni = i + di[d], nj = j + dj[d];
+    if (out(ni, nj, h, w) or a[ni][nj] == '#') continue;
+    dp[ni][nj] += dp[i][j];
+  }
+  cout << dp[h - 1][w - 1].val() << endl;
+  return 0;
 }

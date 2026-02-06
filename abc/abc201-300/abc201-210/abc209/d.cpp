ef63831098b8721ad12f93ff23564792237ccfde
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-06 09:39:06 +0900

    ABC209 D

--- abc/abc201-300/abc201-210/abc209/d.cpp
+++ abc/abc201-300/abc201-210/abc209/d.cpp
@@ -1,13 +1,59 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, q;
+  cin >> n >> q;
+  vector<vector<int>> g(n);
+  rep(_, 0, n - 1) {
+    int a, b;
+    cin >> a >> b;
+    a--, b--;
+    g[a].push_back(b), g[b].push_back(a);
+  }
+  vector<int> color(n, -1);
+  queue<int> que;
+  color[0] = 0;
+  que.push(0);
+  while (!que.empty()) {
+    int u = que.front();
+    que.pop();
+    for (int v : g[u]) {
+      if (color[v] != -1) continue;
+      color[v] = (color[u] + 1) % 2;
+      que.push(v);
+    }
+  }
+  rep(_, 0, q) {
+    int c, d;
+    cin >> c >> d;
+    c--, d--;
+    if ((color[c] + color[d]) % 2 == 0)
+      cout << "Town" << endl;
+    else
+      cout << "Road" << endl;
+  }
+  return 0;
 }

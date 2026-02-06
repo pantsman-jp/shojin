a9b3d2ade987e45d2151dbcebf3a80da37082e70
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-06 16:13:35 +0900

    ABC189,190 D

--- abc/abc101-200/abc181-190/abc189/d.cpp
+++ abc/abc101-200/abc181-190/abc189/d.cpp
@@ -1,13 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector dp(n + 1, vector<ll>(2));
+  dp[0][0] = dp[0][1] = 1;
+  rep(i, 0, n) {
+    string s;
+    cin >> s;
+    if (s == "AND") {
+      dp[i + 1][1] = dp[i][1];
+      dp[i + 1][0] = dp[i][0] * 2 + dp[i][1];
+    } else {
+      dp[i + 1][0] = dp[i][0];
+      dp[i + 1][1] = dp[i][1] * 2 + dp[i][0];
+    }
+  }
+  cout << dp[n][1] << endl;
+  return 0;
 }
--- abc/abc101-200/abc181-190/abc190/d.cpp
+++ abc/abc101-200/abc181-190/abc190/d.cpp
@@ -1,13 +1,45 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+vector<ll> divisors(ll n) {
+  vector<ll> ans;
+  for (ll i = 1; i * i <= n; i++) {
+    if (n % i != 0) continue;
+    ans.push_back(i);
+    if (i != n / i) ans.push_back(n / i);
+  }
+  sort(all(ans));
+  return ans;
+}
+
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  ll n;
+  cin >> n;
+  int ans = 0;
+  for (ll x : divisors(2 * n))
+    if ((x % 2) != ((2 * n / x) % 2)) ans++;
+  cout << ans << endl;
+  return 0;
 }

76aeae26f733b08bec4713f5bec6c0cc2a6cc21b
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-06 12:43:38 +0900

    ABC197 D

--- abc/abc101-200/abc191-200/abc197/d.cpp
+++ abc/abc101-200/abc191-200/abc197/d.cpp
@@ -1,13 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n;
+  ld x, y;
+  complex<ld> a, b;
+  cin >> n;
+  cin >> x >> y;
+  a = {x, y};
+  cin >> x >> y;
+  b = {x, y};
+  auto m = (a + b) / (ld)2.0;
+  auto ans = m + (a - m) * polar((ld)1.0, pi * 2.0 / n);
+  dout(ans.real());
+  cout << " ";
+  dout(ans.imag());
+  cout << endl;
+  return 0;
 }

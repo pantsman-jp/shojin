95f468ebdc1c04ead77e3d2b36546f17797b52f7
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-07 15:22:26 +0900

    ABC180 D

--- abc/abc101-200/abc171-180/abc180/d.cpp
+++ abc/abc101-200/abc171-180/abc180/d.cpp
@@ -1,13 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  ll x, y, a, b;
+  cin >> x >> y >> a >> b;
+  ll cnt = 0;
+  while (true) {
+    if (y / a < x) break;
+    if (a * x >= y) break;
+    if (a * x >= x + b) break;
+    x *= a;
+    cnt++;
+  }
+  cout << cnt + (y - 1 - x) / b << endl;
+  return 0;
 }

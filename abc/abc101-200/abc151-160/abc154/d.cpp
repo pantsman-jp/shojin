8e98363c88ac442d0643af814f0053bbbd9b7b74
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-12 15:52:22 +0900

    ABC153,154 D

--- abc/abc101-200/abc151-160/abc154/d.cpp
+++ abc/abc101-200/abc151-160/abc154/d.cpp
@@ -1,13 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+ld sum(int n) { return n * (n + 1) / 2.0; }
+
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, k;
+  cin >> n >> k;
+  vector<ld> e(n);
+  rep(i, 0, n) {
+    int p;
+    cin >> p;
+    e[i] = sum(p) / (p * 1.0);
+  }
+  vector<ld> sum(n + 1);
+  rep(i, 0, n) sum[i + 1] = sum[i] + e[i];
+  ld ans = -1.0;
+  rep(i, 0, n - k + 1) chmax(ans, sum[i + k] - sum[i]);
+  dout(ans);
+  return 0;
 }

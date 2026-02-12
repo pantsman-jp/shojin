0c338be1e714626b7f0ff4fc75279e4cc081d8c5
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-12 15:33:55 +0900

    ABC156 D

--- abc/abc101-200/abc151-160/abc156/d.cpp
+++ abc/abc101-200/abc151-160/abc156/d.cpp
@@ -1,13 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n");
+#define dout(x) cout << fixed << setprecision(10) << x << "\n";
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+using mint = modint1000000007;
 
-int main()
-{
+mint modpow(ll a, ll b) {
+  mint x = a, ans = 1;
+  while (b > 0) {
+    if (b & 1) ans *= x;
+    x *= x;
+    b >>= 1;
+  }
+  return ans;
+}
+
+mint ncr(ll n, ll r) {
+  if (r < 0 or r > n) return 0;
+  if (r > n - r) r = n - r;
+  mint num = 1, den = 1;
+  rep(i, 0, r) {
+    num *= n - i;
+    den *= i + 1;
+  }
+  return num / den;
+}
+
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  ll n, a, b;
+  cin >> n >> a >> b;
+  mint ans = modpow(2, n);
+  ans -= ncr(n, a);
+  ans -= ncr(n, b);
+  ans--;
+  cout << ans.val() << endl;
+  return 0;
 }
--- utils.cpp
+++ utils.cpp
@@ -18,7 +18,7 @@ using pll = pair<ll, ll>;
 const int inf = 1 << 30;
 const ll INF = 1LL << 60;
 const ld pi = acosl(-1.0L);
-// using mint = modint998244353;
+using mint = modint998244353;
 // using mint = modint1000000007;
 
 // 素数判定
@@ -117,17 +117,45 @@ ll ncr(ll n, ll r) {
 
 ll nc2(ll n) { return n * (n - 1) / 2; }
 
-// 余りの計算(modpow は pow(a,b)(mod m), div は a÷b (mod m) を計算する)
-ll modpow(ll a, ll b, ll mod) {
-  ll ans = 1;
+// 余りの計算
+// pow(a,b) % mod
+mint modpow(ll a, ll b) {
+  mint x = a, ans = 1;
   while (b > 0) {
-    if ((b & 1) != 0) ans = ans * a % mod;
-    a = a * a % mod, b >>= 1;
+    if (b & 1) ans *= x;
+    x *= x, b >>= 1;
   }
   return ans;
 }
 
-ll div(ll a, ll b, ll mod) { return (a * modpow(b, mod - 2, mod)) % mod; }
+// (a÷b) % mod
+mint modiv(ll a, ll b) { return mint(a) * mint(b).inv(); }
+
+// 階乗: n!
+mint modfact(int n) {
+  mint ans = 1;
+  rep(i, 1, n + 1) ans *= i;
+  return ans;
+}
+
+// 順列: nPr
+mint modnpr(ll n, ll r) {
+  mint ans = 1;
+  rep(i, 0, r) ans *= (n - i);
+  return ans;
+}
+
+// 組み合わせ: nCr
+mint modncr(ll n, ll r) {
+  if (r < 0 or r > n) return 0;
+  if (r > n - r) r = n - r;
+  mint num = 1, den = 1;
+  rep(i, 0, r) num *= (n - i), den *= (i + 1);
+  return num / den;
+}
+
+// 組み合わせ: nC2
+mint modnc2(ll n) { return mint(n) * (n - 1) / 2; }
 
 // 平方数判別
 bool is_square(ll n) {
@@ -151,9 +179,6 @@ int digit_sum(ll n) {
   while (n > 0) ans += n % 10, n /= 10;
   return ans;
 }
-
-// 桁数
-int digit(ll n) { return to_string(n).size(); }
 
 // 4 近傍（時計回り）
 const vector<int> di = {-1, 0, 1, 0};
@@ -188,20 +213,6 @@ bool is_pali(string s) {
 }
 
 // ラングレス圧縮
-// string rl(string s)
-// {
-//   int cnt = 1;
-//   string ans;
-//   rep(i, 0, s.size())
-//   {
-//     if (i + 1 < s.size() and s[i] == s[i + 1])
-//       cnt++;
-//     else
-//       ans += string(1, s[i]) + to_string(cnt), cnt = 1;
-//   }
-//   return ans;
-// }
-
 vector<pair<char, int>> rle(string s) {
   vector<pair<char, int>> ans;
   int cnt = 1;
@@ -223,24 +234,19 @@ int count_char(string s, char c) {
   return ans;
 }
 
-// i 番目のアルファベット (0-index)
-char ith_alph(int i) { return 'a' + i; }
-
 // x 桁になるまで自然数 n を 文字 c で埋める
-string pad(int x, int n, char c) {
+string padding(int x, int n, char c) {
   string s = to_string(n);
   return string(max(0, x - (int)s.size()), c) + s;
 }
 
 // ユークリッド距離の二乗
-ll eudistance(ll x1, ll y1, ll x2, ll y2) {
+ll eudist(ll x1, ll y1, ll x2, ll y2) {
   return (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
 }
 
 // マンハッタン距離
-ll mandistance(ll x1, ll y1, ll x2, ll y2) {
-  return abs(x1 - x2) + abs(y1 - y2);
-}
+ll mandist(ll x1, ll y1, ll x2, ll y2) { return abs(x1 - x2) + abs(y1 - y2); }
 
 // ダイクストラ法
 vector<ll> dijkstra(const vector<vector<pair<int, ll>>>& g, int s) {

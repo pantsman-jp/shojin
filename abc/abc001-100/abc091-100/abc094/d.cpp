94a45752ed782cb9a55b38e4d1aff7f015e6d30f
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-16 15:48:37 +0900

    ABC094,109 D

--- abc/abc001-100/abc091-100/abc094/d.cpp
+++ abc/abc001-100/abc091-100/abc094/d.cpp
@@ -1,13 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n")
+#define dout(x) cout << fixed << setprecision(10) << x << "\n"
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<int> a(n);
+  rep(i, 0, n) cin >> a[i];
+  sort(all(a));
+  int ai = a.back(), aj = -1, tar = ai / 2;
+  auto it = lower_bound(a.begin(), a.end() - 1, tar);
+  if (it == a.begin())
+    aj = *it;
+  else if (it == a.end() - 1)
+    aj = *(it - 1);
+  else {
+    int r1 = *(it - 1), r2 = *it;
+    if (abs(r1 - tar) < abs(r2 - tar))
+      aj = r1;
+    else
+      aj = r2;
+  }
+  cout << ai << " " << aj << endl;
+  return 0;
 }
--- abc/abc101-200/abc101-110/abc109/d.cpp
+++ abc/abc101-200/abc101-110/abc109/d.cpp
@@ -1,13 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n")
+#define dout(x) cout << fixed << setprecision(10) << x << "\n"
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int h, w;
+  cin >> h >> w;
+  vector a(h, vector<int>(w));
+  rep(i, 0, h) rep(j, 0, w) cin >> a[i][j], a[i][j] %= 2;
+  vector<tuple<int, int, int, int>> ans;
+  rep(i, 0, h) rep(j, 0, w) {
+    if (a[i][j] == 0) continue;
+    a[i][j] = 0;
+    if (j != w - 1) {
+      a[i][j + 1] = (a[i][j + 1] + 1) % 2;
+      ans.emplace_back(i + 1, j + 1, i + 1, j + 2);
+    } else if (i != h - 1) {
+      a[i + 1][j] = (a[i + 1][j] + 1) % 2;
+      ans.emplace_back(i + 1, j + 1, i + 2, j + 1);
+    }
+  }
+  cout << ans.size() << endl;
+  for (auto [a, b, c, d] : ans)
+    cout << a << " " << b << " " << c << " " << d << endl;
+  return 0;
 }

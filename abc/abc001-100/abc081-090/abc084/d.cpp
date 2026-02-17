ca1b52744eb533343775061c7f3d78f3b788fd4e
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-17 12:33:09 +0900

    ABC084,085 D

--- abc/abc001-100/abc081-090/abc084/d.cpp
+++ abc/abc001-100/abc081-090/abc084/d.cpp
@@ -1,13 +1,60 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
-using P = pair<int, int>;
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n")
+#define dout(x) cout << fixed << setprecision(10) << x << "\n"
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+vector<ll> primes(ll n) {
+  vector<bool> prime(n + 2, true);
+  prime[0] = prime[1] = false;
+  for (ll i = 2; i * i <= n; i++) {
+    if (prime[i])
+      for (ll x = i * i; x <= n; x += i) prime[x] = false;
+  }
+  vector<ll> ans;
+  for (ll i = 2; i <= n; i++)
+    if (prime[i]) ans.push_back(i);
+  return ans;
+}
+
+bool is_prime(ll n) {
+  if (n < 2) return false;
+  for (ll i = 2; i * i <= n; i++)
+    if (n % i == 0) return false;
+  return true;
+}
+
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  vector<ll> like;
+  for (ll n : primes(100000))
+    if (n % 2 != 0 and is_prime((n + 1) / 2)) like.push_back(n);
+  int q;
+  cin >> q;
+  rep(_, 0, q) {
+    ll l, r;
+    cin >> l >> r;
+    int i = lower_bound(all(like), l) - like.begin();
+    int j = upper_bound(all(like), r) - like.begin();
+    cout << j - i << endl;
+  }
+  return 0;
 }

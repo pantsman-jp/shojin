13bcbf30bc5d747900df3c5f3c232cdb33261422
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-20 15:04:23 +0900

    ABC420,428 E

--- abc/abc401-500/abc411-420/abc420/e.cpp
+++ abc/abc401-500/abc411-420/abc420/e.cpp
@@ -1,13 +1,58 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <string>
-#include <cmath>
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n")
+#define dout(x) cout << fixed << setprecision(10) << x << "\n"
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, q;
+  cin >> n >> q;
+  dsu uf(n);
+  vector<int> c(n), s(n);
+  rep(_, 0, q) {
+    int t, u, v;
+    cin >> t;
+    if (t == 1) {
+      cin >> u >> v;
+      u--, v--;
+      u = uf.leader(u);
+      v = uf.leader(v);
+      if (u != v) {
+        uf.merge(u, v);
+        int w = uf.leader(u);
+        s[w] = s[u] + s[v];
+        s[u ^ v ^ w] = 0;
+      }
+    } else if (t == 2) {
+      cin >> u;
+      u--;
+      s[uf.leader(u)] -= c[u];
+      c[u] ^= 1;
+      s[uf.leader(u)] += c[u];
+    } else {
+      cin >> u;
+      u--;
+      yn(s[uf.leader(u)] != 0);
+    }
+  }
+  return 0;
 }
--- abc/abc401-500/abc421-430/abc428/e.cpp
+++ abc/abc401-500/abc421-430/abc428/e.cpp
@@ -1,9 +1,49 @@
 #include <bits/stdc++.h>
-#define rep(i, n) for (ll i = 0; i < (n); i++)
 using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n")
+#define dout(x) cout << fixed << setprecision(10) << x << "\n"
 using ll = long long;
-const ll inf = 1LL << 60;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n;
+  cin >> n;
+  vector<vector<int>> g(n);
+  rep(_, 0, n - 1) {
+    int a, b;
+    cin >> a >> b;
+    a--, b--;
+    g[a].push_back(b), g[b].push_back(a);
+  }
+  vector<pii> ans(n);
+  auto dfs = [&](auto dfs, int sv, int v, int p = -1, int d = 0) -> pii {
+    chmax(ans[v], pii(d, sv));
+    pii res = {d, v};
+    for (int u : g[v])
+      if (u != p) chmax(res, dfs(dfs, sv, u, v, d + 1));
+    return res;
+  };
+  int a = dfs(dfs, 0, 0).second;
+  int b = dfs(dfs, a, a).second;
+  dfs(dfs, b, b);
+  for (auto [_, u] : ans) cout << u + 1 << endl;
+  return 0;
 }

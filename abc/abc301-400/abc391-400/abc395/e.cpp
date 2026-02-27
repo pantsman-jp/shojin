9d78740e907d596c3b103affdc6336770967faf4
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-02-27 14:56:34 +0900

    ABC395E, ABC398F

--- abc/abc301-400/abc391-400/abc395/e.cpp
+++ abc/abc301-400/abc391-400/abc395/e.cpp
@@ -1,23 +1,54 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
-#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
 #define rall(p) p.rbegin(), p.rend()
-#define chmax(a, b) a = max(a, b)
-#define chmin(a, b) a = min(a, b)
-#define yn(p) cout << (p ? "Yes\n" : "No\n");
-#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n")
+#define dout(x) cout << fixed << setprecision(10) << x << "\n"
 using ll = long long;
+using ull = unsigned long long;
 using ld = long double;
-using P = pair<int, int>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 const int inf = 1 << 30;
 const ll INF = 1LL << 60;
-// const ll mod = 998244353;
-// const ll mod = 1000000007;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, m;
+  ll x;
+  cin >> n >> m >> x;
+  vector<vector<int>> g(2 * n);
+  rep(i, 0, m) {
+    int u, v;
+    cin >> u >> v;
+    u--, v--;
+    g[u * 2].push_back(v * 2);
+    g[v * 2 + 1].push_back(u * 2 + 1);
+  }
+  priority_queue<pll, vector<pll>, greater<pll>> q;
+  vector<ll> dist(2 * n, INF);
+  auto push = [&](int v, ll d) {
+    if (dist[v] <= d) return;
+    dist[v] = d;
+    q.emplace(d, v);
+  };
+  push(0, 0);
+  while (!q.empty()) {
+    auto [d, u] = q.top();
+    q.pop();
+    if (dist[u] != d) continue;
+    for (int v : g[u]) push(v, d + 1);
+    push(u ^ 1, d + x);
+  }
+  cout << min(dist[2 * (n - 1)], dist[2 * (n - 1) + 1]) << '\n';
 }
--- abc/abc301-400/abc391-400/abc398/f.cpp
+++ abc/abc301-400/abc391-400/abc398/f.cpp
@@ -1,9 +1,43 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n")
+#define dout(x) cout << fixed << setprecision(10) << x << "\n"
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string s;
+  cin >> s;
+  string t = s;
+  reverse(all(t));
+  t += s;
+  vector<int> z = z_algorithm(t);
+  int n = s.size();
+  rep(i, 0, n) {
+    if (z[n + i] == n - i) {
+      string ans = s;
+      ans += t.substr(n - i, i);
+      cout << ans << endl;
+      return 0;
+    }
+  }
+  return 0;
 }

2ca428b6e66fa02e6c41e0e46ea5b57a2a924f96
Pantsman <214776175+pantsman-jp@users.noreply.github.com>
2026-03-30 12:59:17 +0900

    ABC448-450 C

--- abc/abc401-500/abc441-450/abc448/c.cpp
+++ abc/abc401-500/abc441-450/abc448/c.cpp
@@ -1,23 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
-#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
 #define rall(p) p.rbegin(), p.rend()
-#define chmax(a, b) a = max(a, b)
-#define chmin(a, b) a = min(a, b)
-#define yn(p) cout << (p ? "Yes\n" : "No\n");
-#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n")
+#define dout(x) cout << fixed << setprecision(10) << x << "\n"
 using ll = long long;
+using ull = unsigned long long;
 using ld = long double;
-using P = pair<int, int>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 const int inf = 1 << 30;
 const ll INF = 1LL << 60;
-// const ll mod = 998244353;
-// const ll mod = 1e9 + 7;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int n, q;
+  cin >> n >> q;
+  vector<pii> a(n);
+  rep(i, 0, n) {
+    cin >> a[i].first;
+    a[i].second = i;
+  }
+  sort(all(a));
+  vector<bool> remove(n);
+  rep(_, 0, q) {
+    int k;
+    cin >> k;
+    vector<int> b(k);
+    rep(i, 0, k) {
+      cin >> b[i];
+      remove[b[i] - 1] = true;
+    }
+    rep(i, 0, k + 1) {
+      if (!remove[a[i].second]) {
+        cout << a[i].first << '\n';
+        break;
+      }
+    }
+    rep(i, 0, k) remove[b[i] - 1] = false;
+  }
+  return 0;
 }
--- abc/abc401-500/abc441-450/abc450/c.cpp
+++ abc/abc401-500/abc441-450/abc450/c.cpp
@@ -1,23 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define rep(i, a, b) for (int i = (a); i < (b); i++)
-#define rrep(i, a, b) for (int i = (a) - 1; i >= b; i--)
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
 #define all(p) p.begin(), p.end()
 #define rall(p) p.rbegin(), p.rend()
-#define chmax(a, b) a = max(a, b)
-#define chmin(a, b) a = min(a, b)
-#define yn(p) cout << (p ? "Yes\n" : "No\n");
-#define dout(f) cout << fixed << setprecision(10) << f << '\n';
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n")
+#define dout(x) cout << fixed << setprecision(10) << x << "\n"
 using ll = long long;
+using ull = unsigned long long;
 using ld = long double;
-using P = pair<int, int>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 const int inf = 1 << 30;
 const ll INF = 1LL << 60;
-// const ll mod = 998244353;
-// const ll mod = 1e9 + 7;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
 
-int main()
-{
+int main() {
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
+  int h, w;
+  cin >> h >> w;
+  vector<string> s(h);
+  rep(i, 0, h) cin >> s[i];
+  dsu uf(h * w);
+  rep(i, 0, h) rep(j, 0, w - 1) if (s[i][j] == '.' and s[i][j + 1] == '.') {
+    uf.merge(i * w + j, i * w + (j + 1));
+  }
+  rep(i, 0, h - 1) rep(j, 0, w) if (s[i][j] == '.' and s[i + 1][j] == '.') {
+    uf.merge(i * w + j, (i + 1) * w + j);
+  }
+  set<int> all, out;
+  rep(i, 0, h) rep(j, 0, w) if (s[i][j] == '.') {
+    all.insert(uf.leader(i * w + j));
+    if (i == 0 or i == h - 1 or j == 0 or j == w - 1)
+      out.insert(uf.leader(i * w + j));
+  }
+  cout << all.size() - out.size() << '\n';
+  return 0;
 }

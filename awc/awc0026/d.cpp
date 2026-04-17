66b929db2ee098f78d8159ea41d05e1d8d204284
pantsman-jp <214776175+pantsman-jp@users.noreply.github.com>
2026-04-17 11:13:49 +0900

    AWC0026

---
+++ awc/awc0026/d.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+#define rep(i, l, r) for (int i = (l); i < (r); i++)
+#define rrep(i, l, r) for (int i = (r) - 1; i >= (l); i--)
+#define all(p) p.begin(), p.end()
+#define rall(p) p.rbegin(), p.rend()
+#define chmax(x, y) x = max(x, y)
+#define chmin(x, y) x = min(x, y)
+#define yn(tf) cout << (tf ? "Yes\n" : "No\n")
+#define dout(x) cout << fixed << setprecision(10) << x << "\n"
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+const int inf = 1 << 30;
+const ll INF = 1LL << 60;
+const ld pi = acosl(-1.0L);
+// using mint = modint998244353;
+// using mint = modint1000000007;
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, k;
+  cin >> n >> k;
+  vector<pii> events;
+  rep(i, 0, n) {
+    int l, r;
+    cin >> l >> r;
+    events.push_back({l, 1});
+    events.push_back({r, -1});
+  }
+  sort(all(events));
+  int ans = 0, cnt = 0;
+  rep(i, 0, events.size() - 1) {
+    cnt += events[i].second;
+    if (cnt >= k) ans += events[i + 1].first - events[i].first;
+  }
+  cout << ans << '\n';
+  return 0;
+}
